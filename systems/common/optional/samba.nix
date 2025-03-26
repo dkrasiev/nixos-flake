4b820fa44783308807d995bdaf334d397ac1a597
Dmitry Krasiev <dkrasiev@datafire.ru>
2025-03-26 10:21:56 +0400

    feat(common): configure samba

--- systems/common/optional/samba.nix
+++ systems/common/optional/samba.nix
@@ -1,3 +1,20 @@
 {
-  services.samba.enable = true;
+  services.samba = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+      "public" = {
+        "path" = "/home/dkrasiev/Public";
+        "browseable" = "yes";
+        "read only" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+      };
+    };
+  };
+
+  services.samba-wsdd = {
+    enable = true;
+    openFirewall = true;
+  };
 }
