7241c7ae118663ec5e963611c5c32f149236b707
Dmitry Krasiev <dkrasiev@datafire.ru>
2025-04-08 10:19:48 +0400

    feat(systems/common): add amnezia-wg module, update amnezia-vpn module

--- systems/common/optional/amnezia-vpn.nix
+++ systems/common/optional/amnezia-vpn.nix
@@ -1,9 +1,15 @@
 { outputs, ... }:
 
+let
+  pkgs = outputs.pkgs-unstable;
+in
 {
-  environment.systemPackages = [
-    outputs.pkgs-unstable.amnezia-vpn
-    outputs.pkgs-unstable.amneziawg-tools
-    outputs.pkgs-unstable.amneziawg-go
-  ];
+  # NOTE: copy from https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/programs/amnezia-vpn.nix
+  environment.systemPackages = [ pkgs.amnezia-vpn ];
+  services.dbus.packages = [ pkgs.amnezia-vpn ];
+  services.resolved.enable = true;
+  systemd = {
+    packages = [ pkgs.amnezia-vpn ];
+    services."AmneziaVPN".wantedBy = [ "multi-user.target" ];
+  };
 }
