cdc2dc717a3ade34964e452c2a8136f16d26aa64
Dmitry Krasiev <dkrasiev@datafire.ru>
2025-04-23 09:07:29 +0300

    docs: add comment for amnezia-vpn service

--- systems/common/optional/amnezia-vpn.nix
+++ systems/common/optional/amnezia-vpn.nix
@@ -5,6 +5,8 @@ let
 in
 {
   # NOTE: copy from https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/programs/amnezia-vpn.nix
+  # TODO: replace with services.amnezia-vpn.enable = true;
+  # after >24.11 nixos update
   environment.systemPackages = [ pkgs.amnezia-vpn ];
   services.dbus.packages = [ pkgs.amnezia-vpn ];
   services.resolved.enable = true;
