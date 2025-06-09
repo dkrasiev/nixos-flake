bcdd2cbee34f60ccda02d42369483e9dcb14ad5e
Dmitry Krasiev <dkrasiev@datafire.ru>
2025-06-09 11:13:33 +0400

    feat(systems/common): open ssh port

--- systems/common/optional/openssh.nix
+++ systems/common/optional/openssh.nix
@@ -1,13 +1,17 @@
-{
+let port = 22;
+in {
   services.openssh = {
     enable = true;
-    ports = [ 22 ];
+    ports = [ port ];
     settings = {
       PasswordAuthentication = true;
       # AllowUsers = null; # Allows all users by default. Can be [ "user1" "user2" ]
       # UseDns = true;
       X11Forwarding = true;
-      PermitRootLogin = "prohibit-password"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
+      PermitRootLogin =
+        "prohibit-password"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
     };
   };
+
+  networking.firewall.allowedTCPPorts = [ port ];
 }
